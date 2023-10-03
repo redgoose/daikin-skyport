40c760b008e6e25cd7ed840025df08629d1c9463
Tariq <chill.tariq@gmail.com>
2023-10-02 22:27:51 -0400

    move httpClient and urlBase into constructor

--- daikin/daikin.go
+++ daikin/daikin.go
@@ -11,10 +11,12 @@ import (
 )
 
 type Daikin struct {
-	Email       string
-	Password    string
-	tokenCache  *Token
-	tokenExpiry time.Time
+	Email          string
+	Password       string
+	tokenCache     *Token
+	tokenExpiresAt time.Time
+	httpClient     *http.Client
+	urlBase        string
 }
 
 type Token struct {
@@ -93,20 +95,19 @@ type DeviceInfo struct {
 	Mode                   Mode              `json:"mode"`
 }
 
-var httpClient = &http.Client{Timeout: 10 * time.Second}
-var urlBase string = "https://api.daikinskyport.com"
-
 func New(email string, password string) *Daikin {
 	d := Daikin{
-		Email:    email,
-		Password: password,
+		Email:      email,
+		Password:   password,
+		httpClient: &http.Client{Timeout: 10 * time.Second},
+		urlBase:    "https://api.daikinskyport.com",
 	}
 	return &d
 }
 
 func (d *Daikin) getToken() (string, error) {
 
-	if d.tokenCache != nil && time.Now().Before(d.tokenExpiry) {
+	if d.tokenCache != nil && time.Now().Before(d.tokenExpiresAt) {
 		return d.tokenCache.AccessToken, nil
 	}
 
@@ -115,14 +116,14 @@ func (d *Daikin) getToken() (string, error) {
 		"password": "` + d.Password + `"
 	}`)
 
-	r, err := http.NewRequest("POST", urlBase+"/users/auth/login", bytes.NewBuffer(body))
+	r, err := http.NewRequest("POST", d.urlBase+"/users/auth/login", bytes.NewBuffer(body))
 	if err != nil {
 		return "", errors.New("http.NewRequest failed")
 	}
 
 	r.Header.Add("content-type", "application/json")
 
-	res, err := httpClient.Do(r)
+	res, err := d.httpClient.Do(r)
 	if err != nil {
 		return "", errors.New("http request failed")
 	}
@@ -140,13 +141,13 @@ func (d *Daikin) getToken() (string, error) {
 	}
 
 	d.tokenCache = token
-	d.tokenExpiry = time.Now().Add(time.Duration(token.AccessTokenExpiresIn) * time.Second)
+	d.tokenExpiresAt = time.Now().Add(time.Duration(token.AccessTokenExpiresIn) * time.Second)
 
 	return token.AccessToken, nil
 }
 
 func (d *Daikin) GetDevices() (*Devices, error) {
-	r, err := http.NewRequest("GET", urlBase+"/devices", nil)
+	r, err := http.NewRequest("GET", d.urlBase+"/devices", nil)
 	if err != nil {
 		return nil, errors.New("http.NewRequest failed")
 	}
@@ -160,7 +161,7 @@ func (d *Daikin) GetDevices() (*Devices, error) {
 
 	r.Header.Add("Authorization", "Bearer "+token)
 
-	res, err := httpClient.Do(r)
+	res, err := d.httpClient.Do(r)
 	if err != nil {
 		return nil, errors.New("http request failed")
 	}
@@ -181,7 +182,7 @@ func (d *Daikin) GetDevices() (*Devices, error) {
 }
 
 func (d *Daikin) GetDeviceInfo(deviceId string) (*DeviceInfo, error) {
-	r, err := http.NewRequest("GET", urlBase+"/deviceData/"+deviceId, nil)
+	r, err := http.NewRequest("GET", d.urlBase+"/deviceData/"+deviceId, nil)
 	if err != nil {
 		return nil, errors.New("http.NewRequest failed")
 	}
@@ -195,7 +196,7 @@ func (d *Daikin) GetDeviceInfo(deviceId string) (*DeviceInfo, error) {
 
 	r.Header.Add("Authorization", "Bearer "+token)
 
-	res, err := httpClient.Do(r)
+	res, err := d.httpClient.Do(r)
 	if err != nil {
 		return nil, errors.New("http request failed")
 	}
@@ -223,7 +224,7 @@ func (d *Daikin) SetMode(deviceId string, mode Mode) error {
 func (d *Daikin) UpdateDevice(deviceId string, json string) error {
 	body := []byte(json)
 
-	r, err := http.NewRequest("PUT", urlBase+"/deviceData/"+deviceId, bytes.NewBuffer([]byte(body)))
+	r, err := http.NewRequest("PUT", d.urlBase+"/deviceData/"+deviceId, bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		return errors.New("http.NewRequest failed")
 	}
@@ -237,7 +238,7 @@ func (d *Daikin) UpdateDevice(deviceId string, json string) error {
 
 	r.Header.Add("Authorization", "Bearer "+token)
 
-	res, err := httpClient.Do(r)
+	res, err := d.httpClient.Do(r)
 	if err != nil {
 		return errors.New("http request failed")
 	}
